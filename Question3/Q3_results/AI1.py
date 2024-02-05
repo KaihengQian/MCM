9e6553828e4a28bc88bd1033451f50d8ae5a9a01
Minecraft_zzz <2806700029@qq.com>
2024-02-05 16:17:15 +0800

    Update

--- Question3/Q3_results/AI1.py
+++ Question3/Q3_results/AI1.py
@@ -3,8 +3,3 @@ def calculate_p1(value):
     result_list = []
     result_list.append(value)
     return result_list
-
-def calculate_p1(value):
-    # Correct implementation that creates a new list
-    result_list = [value]
-    return result_list
---
+++ Question3/Q3_results/AI2.py
@@ -0,0 +1,4 @@
+def calculate_p1(value):
+    # Correct implementation that creates a new list
+    result_list = [value]
+    return result_list
